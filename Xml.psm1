7ea8482d49d7760250bd91199e0512c576b2fdda
Joel Bennett <Jaykul@HuddledMasses.org>
2015-07-19 22:59:12 -0400

    Reformat Format-Xml

--- Xml.psm1
+++ Xml.psm1
@@ -203,51 +203,54 @@ Set-Alias sxml Set-XmlContent
 Set-Alias sx Set-XmlContent
 
 function Format-Xml {
-#.Synopsis
-#   Pretty-print formatted XML source
-#.Description
-#   Runs an XmlDocument through an auto-indenting XmlWriter
-#.Parameter Xml
-#   The Xml Document
-#.Parameter Path
-#   The path to an xml document (on disc or any other content provider).
-#.Parameter Indent
-#   The indent level (defaults to 2 spaces)
-#.Example
-#   [xml]$xml = get-content Data.xml
-#   C:\PS>Format-Xml $xml
-#.Example
-#   get-content Data.xml | Format-Xml
-#.Example
-#   Format-Xml C:\PS\Data.xml
-#.Example
-#   ls *.xml | Format-Xml
-#
-[CmdletBinding()]
-param(
-   [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true, ParameterSetName="Document")]
-   [xml]$Xml
-,
-   [Parameter(Position=0, Mandatory=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName="File")]
-   [Alias("PsPath")]
-   [string]$Path
-,
-   [Parameter(Mandatory=$false)]
-   $Indent=2
-)
-process {
-   ## Load from file, if necessary
-   if($Path) { [xml]$xml = Get-Content $Path }
-   
-   $StringWriter = New-Object System.IO.StringWriter
-   $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter
-   $xmlWriter.Formatting = "indented"
-   $xmlWriter.Indentation = $Indent
-   $xml.WriteContentTo($XmlWriter)
-   $XmlWriter.Flush()
-   $StringWriter.Flush()
-   Write-Output $StringWriter.ToString()
-}}
+    #.Synopsis
+    #   Pretty-print formatted XML source
+    #.Description
+    #   Runs an XmlDocument through an auto-indenting XmlWriter
+    #.Example
+    #   [xml]$xml = get-content Data.xml
+    #   C:\PS>Format-Xml $xml
+    #.Example
+    #   get-content Data.xml | Format-Xml
+    #.Example
+    #   Format-Xml C:\PS\Data.xml -indent 1 -char `t
+    #   Shows how to convert the indentation to tabs (which can save bytes dramatically, while preserving readability)
+    #.Example
+    #   ls *.xml | Format-Xml
+    #
+    [CmdletBinding()]
+    param(
+        #   The Xml Document
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$true, ParameterSetName="Document")]
+        [xml]$Xml,
+
+        # The path to an xml document (on disc or any other content provider).
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName="File")]
+        [Alias("PsPath")]
+        [string]$Path,
+
+        # The indent level (defaults to 2 spaces)
+        [Parameter(Mandatory=$false)]
+        [int]$Indent=2,
+
+        # The indent character (defaults to a space)
+        [char]$Character
+    )
+    process {
+        ## Load from file, if necessary
+        if($Path) { [xml]$xml = Get-Content $Path }
+
+        $StringWriter = New-Object System.IO.StringWriter
+        $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter
+        $xmlWriter.Formatting = "indented"
+        $xmlWriter.Indentation = $Indent
+        $xmlWriter.IndentChar = $Character
+        $xml.WriteContentTo($XmlWriter)
+        $XmlWriter.Flush()
+        $StringWriter.Flush()
+        Write-Output $StringWriter.ToString()
+    }
+}
 Set-Alias fxml Format-Xml -EA 0
 Set-Alias fx   Format-Xml -EA 0
 
